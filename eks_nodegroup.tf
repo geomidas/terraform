e07ef1d8fc67922e141d408c2975f3f947a0c128
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-24 23:29:48 +0100

    eks node group add

---
+++ eks_nodegroup.tf
@@ -0,0 +1,22 @@
+resource "aws_eks_node_group" "prod" {
+  cluster_name    = aws_eks_cluster.prod.name
+  node_group_name = "eks_prod"
+  node_role_arn   = aws_iam_role.eks_prod.arn
+  subnet_ids      = ["subnet-004322718acabdb56", "subnet-03683734ad4f920f8", "subnet-0e5b7685321faadcd"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.prod-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.prod-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.prod-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
