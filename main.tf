0a484a927cdc0c223463d992b4487094c2f26ced
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-24 22:18:09 +0100

    eks cluster

--- main.tf
+++ main.tf
@@ -2,14 +2,3 @@ provider "aws" {
   version = "~> 2.0"
   region  = "eu-west-1"
 }
-
-#terraform {
-#  backend "remote" {
-#    hostname      = "app.terraform.io"
-#    organization  = "geomidas"
-#
-#    workspaces {
-#      name = "perfin"
-#    }
-#  }
-#}
