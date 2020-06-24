2d8c27df4e1c0e6f873a2c7744b5a1b40597f7e8
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-24 22:25:36 +0100

    added iam role

--- eks.tf
+++ eks.tf
@@ -1,3 +1,32 @@
+resource "aws_iam_role" "eks_prod" {
+  name = "eks-prod"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "prod-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.eks_prod.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "prod-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.eks_prod.name}"
+}
+
 resource "aws_eks_cluster" "prod" {
   name     = "prod"
   role_arn = "arn:aws:iam::125296811046:user/terraform"
