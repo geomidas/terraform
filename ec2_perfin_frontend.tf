f7a0a8947ffc419b0dda1f259652dd574e1a3c21
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-25 14:19:59 +0100

    Turn on perfin

--- ec2_perfin_frontend.tf
+++ ec2_perfin_frontend.tf
@@ -1,3 +1,18 @@
+# EC2 perfin frontend
+resource "aws_instance" "frontend" {
+    ami           = ami-00b66cbd71b21169f
+    instance_type = "i3.large"
+    key_name      = aws_key_pair.ansible.key_name
+
+    vpc_security_group_ids = [
+        aws_security_group.frontend.id
+    ]
+
+    tags = {
+        Name = "frontend"
+    }
+}
+
 resource "aws_security_group" "frontend" {
     name = "frontend"
     description = "Allow inbound http, https from my IP and all outbound"
@@ -19,4 +34,9 @@ resource "aws_security_group" "frontend" {
         to_port   = 0
         protocol  = "-1"
     }
+}
+
+# Elastic IP
+resource "aws_eip" "frontend" {
+    instance = aws_instance.frontend.id
 }
