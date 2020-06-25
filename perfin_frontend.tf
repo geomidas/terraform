2d1e89d12a82a17fe1a867131564a5e0de88c5ed
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-25 14:15:02 +0100

    Turn on perfin

--- perfin_frontend.tf
+++ perfin_frontend.tf
@@ -1,30 +1,26 @@
-#variable "aws_ami_id" {}
+variable "perfin_frontend_ami_id" {}
 
 # EC2 perfin frontend
-#resource "aws_instance" "frontend" {
-#    ami           = var.aws_ami_id
-#    instance_type = "i3.large"
-#    key_name      = aws_key_pair.ansible.key_name
-#
-#    vpc_security_group_ids = [
-#        aws_security_group.frontend.id
-#    ]
-#
-#    tags = {
-#        Name = "frontend"
-#        "Terraform" = "true"
-#    }
-#
-#    lifecycle {
-#      create_before_destroy = true
-#    }
-#}
-#
-## Elastic IP
-#resource "aws_eip" "frontend" {
-#    instance = aws_instance.frontend.id
-#
-#    tags = {
-#        "Terraform" = "true"
-#    }
-#}
+resource "aws_instance" "frontend" {
+    ami           = var.perfin_frontend_ami_id
+    instance_type = "i3.large"
+    key_name      = aws_key_pair.ansible.key_name
+
+    vpc_security_group_ids = [
+        aws_security_group.frontend.id
+    ]
+
+    tags = {
+        Name = "frontend"
+        "Terraform" = "true"
+    }
+}
+
+# Elastic IP
+resource "aws_eip" "frontend" {
+    instance = aws_instance.frontend.id
+
+    tags = {
+        "Terraform" = "true"
+    }
+}
