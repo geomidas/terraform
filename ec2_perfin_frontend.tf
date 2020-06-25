74142cfa763f031a312450bcd28f8710ea2f9ac5
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-25 19:59:18 +0100

    Ec2 security groups

--- ec2_perfin_frontend.tf
+++ ec2_perfin_frontend.tf
@@ -21,13 +21,13 @@
 #        from_port = 80
 #        to_port = 80
 #        protocol = "tcp"
-#        cidr_blocks = ["80.233.53.33/32"]
+#        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
 #    }
 #    ingress {
 #        from_port = 22
 #        to_port = 22
 #        protocol = "tcp"
-#        cidr_blocks = ["80.233.53.33/32"]
+#        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
 #    }
 #    egress {
 #        from_port = 0
