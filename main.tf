de4d2605623a035344f0af0e131483db2f31db6f
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-25 13:15:43 +0100

    added terraform config

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "geomidas"
+
+    workspaces {
+      name = "perfin"
+    }
+  }
+}
+
 provider "aws" {
   version = "~> 2.0"
   region  = "eu-west-1"
