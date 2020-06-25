4ed488f2eea63acf318fef0733001d5010f97dc7
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-25 14:23:13 +0100

    perfin added

---
+++ ec2_perfin_frontend.tf
@@ -0,0 +1,42 @@
+# EC2 perfin frontend
+resource "aws_instance" "frontend" {
+    ami           = "ami-00b66cbd71b21169f"
+    instance_type = "i3.large"
+    key_name      = aws_key_pair.ansible.key_name
+
+    vpc_security_group_ids = [
+        aws_security_group.frontend.id
+    ]
+
+    tags = {
+        Name = "frontend"
+    }
+}
+
+resource "aws_security_group" "frontend" {
+    name = "frontend"
+    description = "Allow inbound http, https from my IP and all outbound"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["80.233.53.33/32"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["80.233.53.33/32"]
+    }
+    egress {
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+    }
+}
+
+# Elastic IP
+resource "aws_eip" "frontend" {
+    instance = aws_instance.frontend.id
+}
--- eks.tf
+++ eks.tf
@@ -1,56 +1,56 @@
-#resource "aws_iam_role" "eks_prod" {
-#  name = "eks_prod"
-#
-#  assume_role_policy = <<POLICY
-#{
-#  "Version": "2012-10-17",
-#  "Statement": [
-#    {
-#      "Effect": "Allow",
-#      "Principal": {
-#        "Service": "eks.amazonaws.com"
-#      },
-#      "Action": "sts:AssumeRole"
-#    }
-#  ]
-#}
-#POLICY
-#}
-#
-#resource "aws_iam_role_policy_attachment" "prod-AmazonEKSClusterPolicy" {
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-#  role       = aws_iam_role.eks_prod.name
-#}
-#
-#resource "aws_iam_role_policy_attachment" "prod-AmazonEKSServicePolicy" {
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-#  role       = aws_iam_role.eks_prod.name
-#}
-#
-#resource "aws_eks_cluster" "prod" {
-#  name     = "prod"
-#  role_arn = aws_iam_role.eks_prod.arn
-#
-#  vpc_config {
-#    subnet_ids = ["subnet-004322718acabdb56", "subnet-03683734ad4f920f8", "subnet-0e5b7685321faadcd"]
-#  }
-#}
-#
-#output "endpoint" {
-#  value = aws_eks_cluster.prod.endpoint
-#}
-#
-#output "kubeconfig-certificate-authority-data" {
-#  value = aws_eks_cluster.prod.certificate_authority.0.data
-#}
-#
-#data "aws_eks_cluster_auth" "prod" {
-#  name = "eks_prod"
-#}
-#
-#provider "kubernetes" {
-#  host                   = aws_eks_cluster.prod.endpoint
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.prod.certificate_authority.0.data)
-#  token                  = data.aws_eks_cluster_auth.prod.token
-#  load_config_file       = false
-#}
+resource "aws_iam_role" "eks_prod" {
+  name = "eks_prod"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "prod-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_prod.name
+}
+
+resource "aws_iam_role_policy_attachment" "prod-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_prod.name
+}
+
+resource "aws_eks_cluster" "prod" {
+  name     = "prod"
+  role_arn = aws_iam_role.eks_prod.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-004322718acabdb56", "subnet-03683734ad4f920f8", "subnet-0e5b7685321faadcd"]
+  }
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.prod.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.prod.certificate_authority.0.data
+}
+
+data "aws_eks_cluster_auth" "prod" {
+  name = "eks_prod"
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.prod.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.prod.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.prod.token
+  load_config_file       = false
+}
