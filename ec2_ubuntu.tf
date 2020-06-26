514ee4316c131668412afd5910a89f7b9d8ec8d8
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-26 17:08:40 +0100

    lb

--- ec2_ubuntu.tf
+++ ec2_ubuntu.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "ubuntu" {
     instance_type = "t2.micro"
     key_name      = aws_key_pair.ansible.key_name
 
+    count         = 2 
+
     vpc_security_group_ids = [
         aws_security_group.ubuntu.id
     ]
