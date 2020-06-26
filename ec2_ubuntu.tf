6cc81a4a0cad5c142f92e475e30715cc294400e3
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-26 16:16:21 +0100

    added port 80 into SG

--- ec2_ubuntu.tf
+++ ec2_ubuntu.tf
@@ -22,6 +22,12 @@ resource "aws_security_group" "ubuntu" {
         protocol = "tcp"
         cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
     }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
+    }
     egress {
         from_port = 0
         to_port   = 0
