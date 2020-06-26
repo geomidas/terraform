c0c591dc7dae9b1d332c23f983c8842aa39b3f29
Georgios Keramidas <georgiosmidas@gmail.com>
2020-06-26 18:23:09 +0100

    Lb (#35)
    * lb
    
    * corrected az
    
    * shut down aws

--- ec2_sonarqube.tf
+++ ec2_sonarqube.tf
@@ -1,46 +1,46 @@
-resource "aws_instance" "sonarqube" {
-    ami           = "ami-0ff610fbffc63eead"
-    instance_type = "t2.medium"
-    key_name      = aws_key_pair.ansible.key_name
-
-    vpc_security_group_ids = [
-        aws_security_group.sonarqube.id
-    ]
-
-    tags = {
-        Name = "sonarqube"
-    }
-}
-
-resource "aws_security_group" "sonarqube" {
-    name = "sonarqube"
-    description = "Allow inbound http, https from my IP and all outbound"
-
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
-    }
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
-    }
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-# Elastic IP
-resource "aws_eip" "sonarqube" {
-    instance = aws_instance.sonarqube.id
-
-    tags = {
-        Name = "sonarqube"
-    }
-}
+#resource "aws_instance" "sonarqube" {
+#    ami           = "ami-0ff610fbffc63eead"
+#    instance_type = "t2.medium"
+#    key_name      = aws_key_pair.ansible.key_name
+#
+#    vpc_security_group_ids = [
+#        aws_security_group.sonarqube.id
+#    ]
+#
+#    tags = {
+#        Name = "sonarqube"
+#    }
+#}
+#
+#resource "aws_security_group" "sonarqube" {
+#    name = "sonarqube"
+#    description = "Allow inbound http, https from my IP and all outbound"
+#
+#    ingress {
+#        from_port = 80
+#        to_port = 80
+#        protocol = "tcp"
+#        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
+#    }
+#    ingress {
+#        from_port = 22
+#        to_port = 22
+#        protocol = "tcp"
+#        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
+#    }
+#    egress {
+#        from_port   = 0
+#        to_port     = 0
+#        protocol    = "-1"
+#        cidr_blocks = ["0.0.0.0/0"]
+#    }
+#}
+#
+## Elastic IP
+#resource "aws_eip" "sonarqube" {
+#    instance = aws_instance.sonarqube.id
+#
+#    tags = {
+#        Name = "sonarqube"
+#    }
+#}
--- ec2_ubuntu.tf
+++ ec2_ubuntu.tf
@@ -1,39 +1,40 @@
-resource "aws_instance" "ubuntu" {
-    ami           = "ami-099926fbf83aa61ed"
-    instance_type = "t2.micro"
-    key_name      = aws_key_pair.ansible.key_name
-
-    count         = 2 
-
-    vpc_security_group_ids = [
-        aws_security_group.ubuntu.id
-    ]
-
-    tags = {
-        Name = "ubuntu"
-    }
-}
-
-resource "aws_security_group" "ubuntu" {
-    name = "ubuntu"
-    description = "Allow inbound http, https from my IP and all outbound"
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
-    }
-    ingress {
-        from_port = 3000
-        to_port = 3000
-        protocol = "tcp"
-        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
-    }
-    egress {
-        from_port = 0
-        to_port   = 0
-        protocol  = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
+#resource "aws_instance" "ubuntu" {
+#    ami           = "ami-099926fbf83aa61ed"
+#    instance_type = "t2.micro"
+#    key_name      = aws_key_pair.ansible.key_name
+#
+#    count         = 2 
+#
+#    vpc_security_group_ids = [
+#        aws_security_group.ubuntu.id
+#    ]
+#
+#    tags = {
+#        Name = "ubuntu"
+#    }
+#}
+#
+#resource "aws_security_group" "ubuntu" {
+#    name = "ubuntu"
+#    description = "Allow inbound http, https from my IP and all outbound"
+#
+#    ingress {
+#        from_port = 22
+#        to_port = 22
+#        protocol = "tcp"
+#        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
+#    }
+#    ingress {
+#        from_port = 3000
+#        to_port = 3000
+#        protocol = "tcp"
+#        cidr_blocks = ["80.233.53.33/32", "83.71.12.64/32"]
+#    }
+#    egress {
+#        from_port = 0
+#        to_port   = 0
+#        protocol  = "-1"
+#        cidr_blocks = ["0.0.0.0/0"]
+#    }
+#}
+#
